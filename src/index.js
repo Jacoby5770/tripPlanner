f26938b2d1723e99685286f2a524b374eb18a1d6
Jacoby Kang <jacob.kang@vanderbilt.edu>
2018-06-19 16:00:19 -0500

    markers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log("Hello from Javascript");
+
+const mapboxbl = require("mapbox-gl");
+
+mapboxbl.accessToken = 'pk.eyJ1IjoiamFjb2J5NTc3MCIsImEiOiJjamltNTBhcW4wMGZmM3dydGwwZ2lhNGhlIn0.F7JAtWl9iM_ZCXiREWJptA'
+
+const map = new mapboxbl.Map({
+    container: "map",
+    center: [-74.009, 40.705],
+    zoom: 12,
+    style: "mapbox://styles/mapbox/streets-v10"
+});
+
+const marker = document.createElement("div")
+marker.style.height = "32px"
+marker.style.width = "39px"
+marker.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)"
+new mapboxgl.Marker(marker).setLngLat([-74.009151, 40.705086]).addTo(map);
