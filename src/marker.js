8b1d1e977a9899428a5d1c4f3d18caa2ee52ceee
Jacoby Kang <jacob.kang@vanderbilt.edu>
2018-06-19 16:11:00 -0500

    factory module

---
+++ src/marker.js
@@ -0,0 +1,8 @@
+
+function buildMarker(type) {
+    const marker = document.createElement("div")
+    marker.style.height = "32px"
+    marker.style.width = "39px"
+    marker.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)"
+    return marker;
+}
