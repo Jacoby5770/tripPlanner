1e2ced8536866570232a505cc7ef716b8fda4b05
Jacoby Kang <jacob.kang@vanderbilt.edu>
2018-06-19 16:18:41 -0500

    buildmarker

--- src/marker.js
+++ src/marker.js
@@ -1,8 +1,20 @@
 
-function buildMarker(type) {
+function buildMarker(type, coord) {
+
     const marker = document.createElement("div")
     marker.style.height = "32px"
     marker.style.width = "39px"
-    marker.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)"
+
+    if(type === 'activity') {
+        marker.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)"
+        new mapboxgl.Marker(marker).setLngLat(coord).addTo(map);
+    } else if(type === 'hotel') {
+        marker.style.backgroundImage = "url(http://i.imgur.com/D9574Cu.png)"
+        new mapboxgl.Marker(marker).setLngLat(coord).addTo(map);
+    } else if(type === 'restaurant') {
+        marker.style.backgroundImage = "url(http://i.imgur.com/cqR6pUI.png)"
+        new mapboxgl.Marker(marker).setLngLat(coord).addTo(map);
+    }
+
     return marker;
 }
